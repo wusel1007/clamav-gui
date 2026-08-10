4b53a23797f5c07c0dd954fe81bd152e434a286d
Jörg Macedo da Costa Zopes <joerg.zopes@gmx.de>
2026-08-10 15:06:15 +0100

    test

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ cd build
 cmake .. \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=/app \
-  -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH"
 
 cmake --build . -j"$(nproc)"
 cmake --install . --prefix=/app
